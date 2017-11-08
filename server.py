80a48eed23a364077bf0132a2d0779a95dbb95d3
Vishal Yarabandi <vishaljnv9210@gmail.com>
2017-11-07 19:47:14 -0800

    1. Removed old, bottle framework based webserver. 2. Removed bottle.py 3. Removed login and logout code from socket server.

--- server.py
+++ server.py
@@ -57,51 +57,5 @@ def handleClient(con):
         send_data_to_peer(con, response)
 
 
-def constructResponseMessage(errcode, data=None):
-    if errcode == ERRCODE_WRONG_CREDENTIALS:
-        errmsg = ERRMSG_WRONG_CREDENTIALS
-    elif errcode == ERRCODE_CAN_NOT_LOGOUT:
-        errmsg = ERRMSG_CAN_NOT_LOGOUT
-    elif errcode == ERRCODE_REQUEST_SUCCESS:
-        errmsg = ERRMSG_REQUEST_SUCCESS
-    else:
-        errmsg = ERRMSG_UNKNOWN
-
-    return {"errcode":errcode, "errmsg":errmsg, "data":data}
-
-
-def logout(request):
-    db = get_banking_db(mongoConection)
-    users = get_users_collection(db)
-
-    user_name = request.get("user_name")
-    session_id = request.get("session_id")
-
-    user = users.find_one({"user_name":user_name, "session_id":session_id})
-    if not user:
-        return constructResponseMessage(ERRCODE_CAN_NOT_LOGOUT)
-
-    users.update({"user_name":user_name}, {"$unset":{"session_id":0}})
-
-    return constructResponseMessage(ERRCODE_REQUEST_SUCCESS)
-
-
-def login(request):
-    db = get_banking_db(mongoConection)
-    users = get_users_collection(db)
-
-    user_name = request.get("user_name")
-    password = request.get("password")
-
-    user = users.find_one({"user_name":user_name, "password":password})
-    if not user:
-        return constructResponseMessage(ERRCODE_WRONG_CREDENTIALS)
-
-    session_id = str(uuid.uuid4())
-    users.update({"user_name":user_name, "password":password},{"$set":{"session_id":session_id}})
-
-    return constructResponseMessage(ERRCODE_REQUEST_SUCCESS, {"session_id":session_id})
-
-
 if __name__ == '__main__':
     main()
